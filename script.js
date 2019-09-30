7512f87f6ad36d17bf2ec8f3572c60955efd5bdd
Katie <Tatsu@Katies-MacBook.local>
2019-09-30 15:22:50 -0400

    adding function for clicking

--- script.js
+++ script.js
@@ -3,39 +3,29 @@ apiKey = "DVdTwRrvM23YS9YS0w6J2a4d0gR3znn49luN0xOUlKdq"
 apiURL = "https://stream.watsonplatform.net/text-to-speech/api"
 
 
-document.querySelector("#tv").addEventListener("click", function () {
-  console.log("テレビ")
-});
-document.querySelector("#table").addEventListener("click", function () {
-  console.log("テーブル")
-});
-document.querySelector("#couch").addEventListener("click", function () {
-  console.log("ソファ")
-});
-document.querySelector("#plant").addEventListener("click", function () {
-  console.log("植物")
-});
 
-
-// const checkAlert = async function () {
-//   let response = await axios.get(`https://api.adviceslip.com/advice`);
-//   console.log(response.data.slip.advice);
-
-// };
-
-// checkAlert()
-const list = document.querySelector("#aside");
+const list = document.querySelector("#sprite-layer");
 
 
 const checkAlert = async function () {
   let pokeId = Math.floor(Math.random() * 963);
   let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
-  // let pokeNames = response.data
-  // console.log(pokeNames[20])
   const pokeSprite = response.data.sprites.front_default;
-  console.log(pokeSprite)
-  const pokemonSection = document.querySelector('#sprite');
-  pokemonSection.innerHTML = `<img src="${pokeSprite}">`
+
+  let spriteLayer = document.querySelector('#sprite-layer');
+  let pokemonImg = document.createElement("div");
+  spriteLayer.appendChild(pokemonImg);
+  spriteLayer.innerHTML = `<img src="${pokeSprite}">`
+
+
+  // const pokemonImg5 = document.querySelector('#sprite-layer');
+  // pokemonImg5.innerHTML = `<img src="${pokeSprite}">`
+
+
+  // let pokemonImg1 = `<img src="${pokeSprite}">`
+  // pokeNode.appendChild(pokemonImg1)
+
+  // document.querySelector("#sprite-layer").appendChild(pokeNode);
 };
 
 checkAlert()
