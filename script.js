7eb818d79e11981675e9a82d1fa0247463204ac8
Katie <Tatsu@Katies-MacBook.local>
2019-09-30 14:30:45 -0400

    removing room objects with pokemon

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
+// text-to-speech API
+apiKey = "DVdTwRrvM23YS9YS0w6J2a4d0gR3znn49luN0xOUlKdq"
+apiURL = "https://stream.watsonplatform.net/text-to-speech/api"
+
+
 document.querySelector("#tv").addEventListener("click", function () {
-  console.log("tv")
+  console.log("テレビ")
 });
 document.querySelector("#table").addEventListener("click", function () {
-  console.log("table")
+  console.log("テーブル")
 });
 document.querySelector("#couch").addEventListener("click", function () {
-  console.log("couch")
+  console.log("ソファ")
 });
 document.querySelector("#plant").addEventListener("click", function () {
-  console.log("plant")
+  console.log("植物")
 });
+
+
+// const checkAlert = async function () {
+//   let response = await axios.get(`https://api.adviceslip.com/advice`);
+//   console.log(response.data.slip.advice);
+
+// };
+
+// checkAlert()
+const list = document.querySelector("#aside");
+
+
+const checkAlert = async function () {
+  let pokeId = Math.floor(Math.random() * 963);
+  let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+  // let pokeNames = response.data
+  // console.log(pokeNames[20])
+  const pokeSprite = response.data.sprites.front_default;
+  console.log(pokeSprite)
+  const pokemonSection = document.querySelector('#sprite');
+  pokemonSection.innerHTML = `<img src="${pokeSprite}">`
+};
+
+checkAlert()
+
+
+
+
+
+// sprites.front_default
