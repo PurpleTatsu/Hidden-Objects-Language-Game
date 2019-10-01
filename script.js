3c1075150e34f7f5edf0d0952a188bd6bdfa40a3
Katie <Tatsu@Katies-MacBook.local>
2019-10-01 16:33:49 -0400

    trying to add photos to checkbox labels

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// text-to-speech API
-apiKey = "DVdTwRrvM23YS9YS0w6J2a4d0gR3znn49luN0xOUlKdq"
-apiURL = "https://stream.watsonplatform.net/text-to-speech/api"
-
-
-
-
+//randomly generates 7 Pokemon sprites and names
 
 const checkAlert = async function () {
   let pokeId1 = Math.floor(Math.random() * 700);
@@ -14,7 +8,7 @@ const checkAlert = async function () {
 
   let spriteLayer = document.querySelector('#sprite-layer');
   let pokemonImg = document.createElement("div");
-  pokemonImg.classList.add("pokeId1")
+  pokemonImg.classList.add("pokeId1");
   spriteLayer.appendChild(pokemonImg);
   pokemonImg.innerHTML = `<img src="${pokeSprite}">`
 
@@ -23,7 +17,7 @@ const checkAlert = async function () {
   let findListItem = document.createElement("li");
   let pokeName = response.data.name;
   list.appendChild(findList);
-  findList.appendChild(findListItem)
+  findList.appendChild(findListItem);
   findListItem.innerHTML = pokeName;
 
   // pokemon2
@@ -33,12 +27,13 @@ const checkAlert = async function () {
 
 
   let pokemonImg2 = document.createElement("div");
+  pokemonImg2.classList.add("pokeId2");
   spriteLayer.appendChild(pokemonImg2);
   pokemonImg2.innerHTML = `<img src="${pokeSprite2}">`
 
   let findListItem2 = document.createElement("li");
   let pokeName2 = response2.data.name;
-  findList.appendChild(findListItem2)
+  findList.appendChild(findListItem2);
   findListItem2.innerHTML = pokeName2;
 
   // pokemon3
@@ -48,12 +43,13 @@ const checkAlert = async function () {
 
 
   let pokemonImg3 = document.createElement("div");
+  pokemonImg3.classList.add("pokeId3");
   spriteLayer.appendChild(pokemonImg3);
   pokemonImg3.innerHTML = `<img src="${pokeSprite3}">`
 
   let findListItem3 = document.createElement("li");
   let pokeName3 = response3.data.name;
-  findList.appendChild(findListItem3)
+  findList.appendChild(findListItem3);
   findListItem3.innerHTML = pokeName3;
 
   // pokemon4
@@ -63,12 +59,13 @@ const checkAlert = async function () {
 
 
   let pokemonImg4 = document.createElement("div");
+  pokemonImg4.classList.add("pokeId4");
   spriteLayer.appendChild(pokemonImg4);
   pokemonImg4.innerHTML = `<img src="${pokeSprite4}">`
 
   let findListItem4 = document.createElement("li");
   let pokeName4 = response4.data.name;
-  findList.appendChild(findListItem4)
+  findList.appendChild(findListItem4);
   findListItem4.innerHTML = pokeName4;
   // pokemon5
   let pokeId5 = Math.floor(Math.random() * 700);
@@ -77,12 +74,13 @@ const checkAlert = async function () {
 
 
   let pokemonImg5 = document.createElement("div");
+  pokemonImg5.classList.add("pokeId5");
   spriteLayer.appendChild(pokemonImg5);
   pokemonImg5.innerHTML = `<img src="${pokeSprite5}">`
 
   let findListItem5 = document.createElement("li");
   let pokeName5 = response5.data.name;
-  findList.appendChild(findListItem5)
+  findList.appendChild(findListItem5);
   findListItem5.innerHTML = pokeName5;
 
   // pokemon6
@@ -92,46 +90,76 @@ const checkAlert = async function () {
 
 
   let pokemonImg6 = document.createElement("div");
+  pokemonImg6.classList.add("pokeId6");
   spriteLayer.appendChild(pokemonImg6);
   pokemonImg6.innerHTML = `<img src="${pokeSprite6}">`
 
   let findListItem6 = document.createElement("li");
   let pokeName6 = response6.data.name;
-  findList.appendChild(findListItem6)
+  findList.appendChild(findListItem6);
   findListItem6.innerHTML = pokeName6;
-
   // pokemon7
   let pokeId7 = Math.floor(Math.random() * 700);
   let response7 = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeId7}`);
   const pokeSprite7 = response7.data.sprites.front_default;
 
-
   let pokemonImg7 = document.createElement("div");
+  pokemonImg7.classList.add("pokeId7");
   spriteLayer.appendChild(pokemonImg7);
   pokemonImg7.innerHTML = `<img src="${pokeSprite7}">`
 
   let findListItem7 = document.createElement("li");
   let pokeName7 = response7.data.name;
-  findList.appendChild(findListItem7)
+  findList.appendChild(findListItem7);
   findListItem7.innerHTML = pokeName7;
 
-
+  //SCORE
   let pokeId1Button = document.querySelector(".pokeId1");
-
+  let score = 0;
   pokeId1Button.addEventListener('click', async () => {
-    // if (document.querySelector("#button").clicked === true) {
-    //   score++ && pokeSprite.style.display === "none";
-    // }
-    // else { console.log("did you click the pokemon?") }
-    let score = 0;
     score++
     console.log(score, "poke clicked");
 
-  })
+  });
+
+  let pokeId2Button = document.querySelector(".pokeId2");
+  pokeId2Button.addEventListener('click', async () => {
+    score++
+    console.log(score, "poke clicked");
+  });
+
+  let pokeId3Button = document.querySelector(".pokeId3");
+  pokeId3Button.addEventListener('click', async () => {
+    score++
+    console.log(score, "poke clicked");
+  });
+
+  let pokeId4Button = document.querySelector(".pokeId4");
+  pokeId4Button.addEventListener('click', async () => {
+    score++
+    console.log(score, "poke clicked");
+  });
+
+  let pokeId5Button = document.querySelector(".pokeId5");
+  pokeId5Button.addEventListener('click', async () => {
+    score++
+    console.log(score, "poke clicked");
+  });
+
+  let pokeId6Button = document.querySelector(".pokeId6");
+  pokeId6Button.addEventListener('click', async () => {
+    score++
+    console.log(score, "poke clicked");
+  });
+
+  let pokeId7Button = document.querySelector(".pokeId7");
+  pokeId7Button.addEventListener('click', async () => {
+    score++
+    console.log(score, "poke clicked");
+  });
 
 
-};
-
+}
 checkAlert()
 
 
@@ -139,4 +167,11 @@ checkAlert()
 
 
 
-// if clicked score++ and display: none
+// const duckShot = () => {
+//   pokeId.classLast.add("shot")
+//   const blastClean = () => {
+//     body.removeChild(duckDiv)
+//   }
+//   checkWinner();
+// }
+
